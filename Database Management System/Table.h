4c7ddab7e210e66ce548b01fe1de8ba60a462cf2
pas0185 <patrickanthony.sheehan@yahoo.com>
2014-02-06 04:10:33 -0600

    Table class -PS
    put my functions into header file

--- Database Management System/Table.h
+++ Database Management System/Table.h
@@ -5,32 +5,52 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-
-class Table{
+class Table
+{
 private:
 	string _name;
 	int _numFields;
-	vector<Entry> _entries;
+
+	vector<Entry> _vecEntries;	// vector of vectors is an easier implementation...
+	string* _arrEntries[2];		// array of arrays uses contiguous memory, good when searching columns
+	char*** _ptrEntries;		// maintains a pointer to a block of memory containing-
+								//		pointers that point to blocks of memory containing-
+								//			pointers that point to blocks of memory containing-
+								//				chars
+
 public:
 
-	// constructor
-	Table();
-	Table(string name);
-	Table(string name, int numFields);
+// CONSTRUCTOR
+	Table(string name, int numFields=2);// create table
 
-	// accessors
+// ACCESSORS
 	
+	Entry getEntry(int entryID);
+	Entry getEntry(string name, int col=0);
+	
+	string getName() { return _name; }
+	int getNumFields() { return _numFields; }
 
-	// mutators
-	delete
+// MUTATORS
+			// the following return true on success
+	bool addEntry(Entry e);				// insert into
+	bool addEntry(string field1, string field2);
+	bool addEntry(string field1, string field2, string field3);
+	bool addEntry(vector<string> fields);
 
+	void dropTable();
+	bool update(string key, string newVal, int col=0);
+	bool deleteEntry(int row);
+	bool deleteEntry(string key, int col=0);
 	
 };
 struct Entry{
-	vector<string> _fields;
+	vector<string> fields;
 };
+
 
 #endif
