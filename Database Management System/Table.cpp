fd149e6020900e723d96f4aae8cd9823278b8a89
pas0185 <patrickanthony.sheehan@yahoo.com>
2014-02-09 23:00:09 -0600

    Table class compiles, Entry as a class -PS

--- Database Management System/Table.cpp
+++ Database Management System/Table.cpp
@@ -1,12 +1,93 @@
+#include "stdafx.h"
 #include "Table.h"
 
 
-Table::Table(string name, int numCols=2)
+Table::Table(string name, int numCols)
 { // create a new empty table
 	_name = name;
 	_numCols = numCols;
 	_entries.clear();
 }
-struct deezNuts{
-	int waylon_sucks;
-};
+Table::~Table()
+{ // table destructor
+	_entries.clear();  // NOTE: is this needed?
+}
+
+Entry Table::getEntry(unsigned int entryID)
+{ // return the entry at the provided row
+	Entry entry;
+	if (_entries.size() < entryID) 
+	{
+		throw new exception("entryID out of range in getEntry");
+	}
+	else 
+	{
+		entry = _entries.at(entryID);
+	}
+	return entry;
+}
+//Entry Table::getEntry(string name, int col=0){}
+
+void Table::addEntry(Entry e)
+{  // add new entry (row) to the database 
+	_entries.push_back(e);
+
+}
+void Table::addEntry(vector<string> fields)
+{ // add new entry (row) to database with the fields provided 
+	if (fields.size() != _numCols)
+	{
+		throw new exception("in addEntry, incorrect number of fields provided for this table");
+		return;
+	}
+	
+	Entry e;
+	for (unsigned int i = 0; i < fields.size(); i++)
+	{
+		e.fields.push_back(string(fields.at(i)));
+	}
+	_entries.push_back(e);
+}
+
+void Table::dropTable()
+{ // clear entries vector
+	_entries.clear();
+}
+bool Table::update(string key, string newVal, int keyCol, int valCol)
+{ // update an existing entry with a new value, return true on success
+	for (unsigned int i = 0; i < _entries.size(); i++)
+	{
+		Entry entry = _entries.at(i);
+		string currKey = entry.fields.at(keyCol);
+		if (!key.compare(currKey))
+		{ // if the keys match
+			entry.fields.at(valCol) = newVal;
+			return true;
+		}
+	}
+	return false;
+}
+bool Table::deleteEntryRow(unsigned int row)
+{ // delete an existing entry, given the row number (zero-indexed). return true on success
+	if (_entries.size() < row)
+	{
+		return false;
+	}
+	
+	_entries.erase(_entries.begin() + row);
+	return true;
+}
+bool Table::deleteEntry(string key, int keyCol)
+{ // delete an existing entry whose value at the provided column is equal to the key
+	for (vector<Entry>::iterator iter = _entries.begin(); iter != _entries.end(); iter++)
+	{
+		Entry entry = *iter;
+		string currKey = entry.fields.at(keyCol);
+		if (!key.compare(currKey))
+		{
+			_entries.erase(iter);
+			return true;
+		}
+	}
+	return false;
+}
--- Database Management System/Table.h
+++ Database Management System/Table.h
@@ -1,11 +1,11 @@
 // class to represent a table/relation in the database
-
-#ifndef TABLE_H
-#define TABLE_H
+#pragma once
 
 #include <iostream>
 #include <vector>
 #include <string>
+
+#include "Entry.h"
 
 using namespace std;
 
@@ -15,41 +15,32 @@ private:
 	string _name;
 	int _numCols;
 
-	vector<Entry> _entries;	// vector of vectors is an easier implementation...
-	//string* _arrEntries[2];		// array of arrays uses contiguous memory, good when searching columns
-	//char*** _ptrEntries;		// maintains a pointer to a block of memory containing-
-								//		pointers that point to blocks of memory containing-
-								//			pointers that point to blocks of memory containing-
-								//				chars
+	vector<Entry> _entries;
 
 public:
 
-// CONSTRUCTOR
-	Table(string name, int numCols=2);// create table
+	Table(string name, int numCols=2);
+	~Table();
 
 // ACCESSORS
 	
-	Entry getEntry(int entryID);
-	Entry getEntry(string name, int col=0);
+	Entry getEntry(unsigned int entryID);
+	//Entry getEntry(string name, int col=0);
 	
 	string getName() { return _name; }
 	int getNumCols() { return _numCols; }
 
 // MUTATORS
-			// the following return true on success
-	bool addEntry(Entry e);				// insert into
-	bool addEntry(vector<string> fields);
+
+	void addEntry(Entry e);				// insert into
+	void addEntry(vector<string> fields);
 
 	void dropTable();
-	bool update(string key, string newVal, int col=0);
-	bool deleteEntry(int row);
-	bool deleteEntry(string key, int col=0);
+	bool update(string key, string newVal, int keyCol=0, int valCol=1);
+	
+	bool deleteEntryRow(unsigned int row);
+	bool deleteEntry(string key, int keyCol=0);
 
-
+	bool importDB(string fileName); 
 	
 };
-struct Entry{
-	vector<string> fields;
-};
-
-#endif
