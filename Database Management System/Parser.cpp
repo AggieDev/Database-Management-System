ec710e29b1b83fe86c74f9b07a9c8675433ebe52
pas0185 <patrickanthony.sheehan@yahoo.com>
2014-02-13 13:42:09 -0600

    added parser class

---
+++ Database Management System/Parser.cpp
@@ -0,0 +1,24 @@
+//#include "stdafx.h"
+#include "Parser.h"
+
+using namespace std;
+Parser::Parser()
+{
+}
+
+
+Parser::~Parser()
+{
+}
+
+
+vector<string> Parser::readInputLine(std::string line)
+{
+	// split a line of input into a vector of strings
+	return vector<string>();
+}
+
+void Parser::callFunction(std::vector<std::string> input)
+{
+
+}
