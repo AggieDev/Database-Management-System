eb0239ec6cb0853056f049cb4ad97ef5bea1a162
pas0185 <patrickanthony.sheehan@yahoo.com>
2014-02-17 23:24:25 -0600

    fixed readLiteral and added test

--- Database Management System/Parser.h
+++ Database Management System/Parser.h
@@ -38,7 +38,7 @@ public:
     /*-----Eli---*/
     Table projection(std::vector<std::string>input);
     /*-----Eli---*/
-    Table rename(std::vector<std::string>input);
+    //Table rename(std::vector<std::string>input);
     /*-----Eli---*/
     bool ShowCmd(std::vector<std::string>input);
     /*-----Eli---*/
