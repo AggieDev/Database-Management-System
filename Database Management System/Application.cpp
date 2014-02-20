f27ec42ad7aaf8068956639ff8a4994f2e6aae9b
Waylon Brown <waylonjbrown@gmail.com>
2014-02-19 22:52:26 -0600

    created application class with functions

---
+++ Database Management System/Application.cpp
@@ -0,0 +1,71 @@
+#pragma once
+#include "Application.h"
+
+
+Application::Application()
+{
+}
+Application::~Application()
+{
+}
+
+void Application::launch()
+{
+	//application main code here
+}
+
+void Application::createTable(string tableName, vector<string> columnNames)
+{
+	Database::addTable(Table(tableName, columnNames));
+}
+
+Table Application::insertIntoTable(string tableName, Entry& e)
+{
+	Database::getTable(tableName).addEntry(e);
+}
+
+//will change if using GUI
+void Application::showTable(string tableName)
+{
+	Database::getTable(tableName).printTable();
+}
+
+Table Application::unionTable(string firstTable, string secondTable)
+{
+	return Database::setunion(Database::getTable(firstTable), Database::getTable(secondTable));
+}
+
+Table Application::differenceTable(string firstTable, string secondTable)
+{
+	return Database::differenceTable(Database::getTable(firstTable), Database::getTable(secondTable));
+}
+
+Table Application::productTable(string firstTable, string secondTable)
+{
+	return Database::productTable(Database::getTable(firstTable), Database::getTable(secondTable));
+}
+
+void Application::updateTable(string tableName)
+{
+}
+
+void Application::deleteFromTable(string tableName)
+{
+}
+
+void Application::tableSelect(string tableName)
+{
+}
+
+void Application::renameTable(string tableName)
+{
+}
+
+//does this mean write to file?
+void Application::writeTable(string tableName)
+{
+}
+
+void Application::tableProjection()
+{
+}
